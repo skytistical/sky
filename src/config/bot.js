12936c5b6e8cbc946c4b7324c297bd610b909cdd
skytistical <burneremailskzy@gmail.com>
2026-06-23 17:14:26 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "sky made ts",
+        name: "x",
         // Activity type number (0 = Playing).
-        type: 1,
+        type: 5,
       },
     ],
   },
